fee595599b9c32406f9af5ae336bf33325b82863
unknown <1916853003@qq.com>
2019-07-21 23:31:22 +0800

    6

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,21 @@
+package com.mmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+    /**
+     * 用户登录
+     */
+    @RequestMapping(value ="login.do",method = RequestMethod.POST)
+    @ResponseBody
+    public Object login(String username, String password, HttpSession session){
+        return null;
+    }
+}
